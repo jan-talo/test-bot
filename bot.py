80a8cfa53c297af6d902db12e09c734b15cdea36
jan-talo <jan.talo.pi.toki.pona@gmail.com>
2024-06-07 21:56:44 +0900

    仮公開用に整形した

--- bot.py
+++ bot.py
@@ -28,10 +28,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
     print(f'Logged on as {bot.user}!')
-'''
-async def on_ready(self):
-    print(f'Logged on as {self.user}!')
-'''
+
 # メッセージ受信時に動作する処理
 @bot.event
 async def on_message(message):
@@ -51,20 +48,9 @@ async def on_message(message):
         Emoji = "\N{Grinning Cat Face with Smiling Eyes}"
         await message.add_reaction(Emoji)
         await sendMessage.add_reaction(Emoji)
-        await asyncio.sleep(0.5)
-        await sendMessage.edit(content='にゃーん！')
         await asyncio.sleep(10)
-        print(message.reactions)
-        reaction = message.reactions[0]
-        # I do not actually recommend doing this.
-        async for user in reaction.users():
-            await message.channel.send(f'{user} has reacted with {reaction.emoji}!')
-            print(user.id)
-        users = [user async for user in reaction.users()]
-        # users is now a list of User...
-        winner = random.choice(users)
-        await message.channel.send(f'{winner} has won the raffle.')
-    
+        await sendMessage.edit(content='にゃーん！')
+          
     elif text == '!じゃんけん' \
       or text == '!ジャンケン' \
       or text == '!janken':
@@ -76,37 +62,6 @@ async def on_message(message):
         if text == '/おやすみ':
             print('botはコマンドによりログアウトしました')
             await bot.close()
-
-#@bot.event
-#async def on_disconnect():
-#    await bot.logout()
-#    raise SystemExit()
-#    sys.exit()
-
-"""
- @bot.command()
-async def createChannel(ctx, name: str = None, category: str = None):
-    if not name:
-        await ctx.send('チャンネル名を指定してください。')
-        return
-
-    guild = ctx.guild
-
-    if category:
-        category_channel = discord.utils.get(guild.categories, name=category)
-        if category_channel is None:
-            await ctx.send(f'カテゴリー「{category}」が見つかりません。')
-            return
-    else:
-        category_channel = None
-
-    if category_channel:
-        await guild.create_text_channel(name, category=category_channel)
-    else:
-        await guild.create_text_channel(name)
-
-    await ctx.send(f'チャンネル「{name}」が作成されました。') 
-"""
 
 # Botの起動とDiscordサーバーへの接続
 bot.run(TOKEN)
