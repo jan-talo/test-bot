83d29e3ad1c4260048b45385bdb02b8a5987b53a
jan-talo <jan.talo.pi.toki.pona@gmail.com>
2024-06-08 12:42:23 +0900

    様々なスタンプに対応

--- bot.py
+++ bot.py
@@ -17,6 +17,43 @@ import asyncio
 config_data = json.load(open('config.json','r',encoding="utf-8_sig"))
 TOKEN = config_data['TOKEN']
 
+# じゃんけんスタンプ定義
+list_janken = ["guu","choki","paa"]
+list_stump = {
+    'guu': {
+        'stumps': [":right_facing_fist:", ":punch:", ":fist:", ":left_facing_fist:", ":boxing_glove:", ":rock:", ":gem:", ":bricks:", ":curling_stone:", ":moyai:", ":ice_cube:", ":guitar:", ":zero:", ":o2:", ":u7121:"],
+        'weights': [6,6,6,6,5,4,4,4,4,3,2,2,1,1,1]
+    },
+    'choki': {
+        'stumps': [":v:" ,":scissors:" ,":crossed_swords:" ,":white_check_mark:" ,":heavy_check_mark:" ,":ballot_box_with_check:" ,":crab:" ,":lobster:" ,":fingers_crossed:" ,":person_getting_haircut:" ,":woman_getting_haircut:" ,":man_getting_haircut:" ,":clapper:" ,":carpentry_saw:" ,":axe:" ,":knife:" ,":dagger:" ,":aries:" ,":heavy_division_sign:" ,":u5272:" ,":two:"],
+        'weights': [5,5,3,3,2,3,4,4,3,2,2,2,1,1,1,2,2,2,1,1,1]
+    },
+    'paa': {
+        'stumps': [":rightwards_hand:", ":leftwards_hand:", ":wave:", ":hand_splayed:", ":raised_back_of_hand:", ":raised_hand:", ":scroll:", ":page_facing_up:", ":newspaper2:", ":newspaper:", ":hugging:", ":gloves:", ":vulcan:", ":raised_hands:", ":palms_up_together:", ":open_hands:", ":roll_of_paper:", ":dollar:", ":yen:", ":map:", ":page_with_curl:", ":bookmark_tabs:", ":parking:", ":five:", ":hamsa:", ":euro:", ":pound:"],
+        'weights': [3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1]
+    },
+    'victory': {
+        'stumps': [":trophy:", ":medal:", ":military_medal:", ":partying_face:", ":yum:", ":zany_face:", ":innocent:", ":smirk_cat:", ":crown:", ":cherry_blossom:", ":star:", ":sparkles:", ":rosette:", ":first_place:", ":confetti_ball:", ":tada:", ":sparkling_heart:", ":congratulations:", ":white_flower:", ":100:"],
+        'weights': [2,2,2,2,2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2]
+    }
+}
+
+def getText(name):
+    result_text = ""
+    if name in list_janken:
+        index = list_janken.index(name)
+        result_text = ["ぐー ","ちょき ","ぱー "][index]
+    return result_text
+
+def getStump(name):
+    result_stump = ":green_heart:"
+    if name =="vs":
+        result_stump = ":vs:"
+    elif name in list_stump:
+        temp = list_stump[name]
+        result_stump = random.choices(temp['stumps'],weights=temp['weights'])[0]   
+    return result_stump
+
 # 接続に必要なオブジェクトを生成
 intents = discord.Intents.default()
 intents.message_content = True  # Enable message content intent
@@ -31,7 +68,6 @@ async def on_ready():
 # メッセージ受信時に動作する処理
 @bot.event
 async def on_message(message):
- # async def on_message(self, message):
     # Userが発言したらターミナルに 名前(ID): 発言内容 が表示される
     print(f'Message from {message.author} ({message.author.id}): {message.content}')
     text = message.content
@@ -53,12 +89,16 @@ async def on_message(message):
     elif text == '!じゃんけん' \
       or text == '!ジャンケン' \
       or text == '!janken':
-        listJanken = ["ぐー:fist:", "ちょき:v:", "ぱー:hand_splayed:"]
-        result = random.choice(listJanken)
-        await message.channel.send(f'{message.author}さん：　{result}')
+        choice = random.choice(list_janken)
+        reply_text = f'{message.author}さん： {getText(choice)}'
+        reply_text += getStump(choice)
+        await message.channel.send(reply_text)
 
     elif await bot.is_owner(message.author):
-        if text == '/おやすみ':
+        if text == '!jankenpon':
+            sendMessage = await message.channel.send('じゃんけんに参加する方は、')
+            # message.reactions[0]
+        elif text == '/おやすみ':
             await message.channel.send(f'{message.author}さん、おやすみなさい')
             print('botはコマンドによりログアウトしました')
             await bot.close()
